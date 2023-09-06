ea998a566f0c7c27fe62be58510c07096a0e955b
YagoSMN <yago.silva@smn.com.br>
2023-09-06 00:51:16 -0300

    created login page, started costumers pagelist

---
+++ fastBarberTG/Controllers/BarberControlController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fastBarberTG.Controllers
+{
+    public class BarberControlController : Controller
+    {
+        // GET: BarberControl
+        public ActionResult Index() //Lista todos os cortes de cabelo para o dia atual.
+        {
+            return View();
+        }
+    }
+}
---
+++ fastBarberTG/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="fastBarberTG.MvcApplication" Language="C#" %>

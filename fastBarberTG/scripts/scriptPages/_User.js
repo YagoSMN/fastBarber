bcbd7057d8ced2fe05e5a4d895946aa5993e594e
YagoSMN <yago.silva@smn.com.br>
2024-04-03 16:30:27 -0300

    added user controller

---
+++ fastBarberTG/scripts/scriptPages/_User.js
@@ -0,0 +1,32 @@
+﻿window.user = (function () {
+
+    var config = {
+        urls: {
+            showHorario: '',
+        },
+    };
+
+    var init = function ($config) {
+        config = $config;
+    };
+
+    var showHorario = function() {
+        
+        $.get(config.urls.showHorario).done(function(html) {
+            $("#user-requestdiv").show("slow").html(html);
+            $("#user-settings").hide("slow");
+            console.log(html)
+        }).fail(function(msg) {
+            iziToast.error({
+                title: 'Erro!',
+                message: 'Erro na requisição',
+            });
+        });
+    };
+
+    return {
+        init: init,
+        showHorario: showHorario
+    };
+})();
+

f63057b2c7339a5f8df8d3e7e7f7b0bc8a737b3a
YagoSMN <yago.silva@smn.com.br>
2024-04-03 22:24:30 -0300

    added tables horariofunc screen user

--- FastBarber - Procedures/CREATE---.sql
+++ FastBarber - Procedures/CREATE---.sql
@@ -24,4 +24,21 @@ CREATE TABLE FB_HorariosMarc (
 	TempoCorte		NVARCHAR(8)
 );
 
+CREATE TABLE FB_HorarioFunc(
+	Id							INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	Nome_Semana					NVARCHAR(13) NOT NULL,
+	Horario_Inicio				Time NOT NULL,
+	Horario_AlmocoInicio		Time NOT NULL,
+	Horario_AlmocoFim			Time NOT NULL,
+	Horario_Fim					Time NOT NULL,
+	Ind_Ativo						Bit NOT NULL,
+)
+
 INSERT INTO FB_Barber(Nome, Email, Senha) VALUES ('yago', 'yagohenriquest@gmail.com', '123456789123');
+INSERT INTO FB_HorarioFunc(Nome_Semana, Horario_Inicio, Horario_AlmocoInicio, Horario_AlmocoFim, Horario_Fim, Ind_Ativo) VALUES ('Segunda-Feira', '00:00:00', '00:00:00','00:00:00','00:00:00', 1)
+INSERT INTO FB_HorarioFunc(Nome_Semana, Horario_Inicio, Horario_AlmocoInicio, Horario_AlmocoFim, Horario_Fim, Ind_Ativo) VALUES ('Terça-Feira', '00:00:00', '00:00:00','00:00:00','00:00:00', 1)
+INSERT INTO FB_HorarioFunc(Nome_Semana, Horario_Inicio, Horario_AlmocoInicio, Horario_AlmocoFim, Horario_Fim, Ind_Ativo) VALUES ('Quarta-Feira', '00:00:00', '00:00:00','00:00:00','00:00:00', 1)
+INSERT INTO FB_HorarioFunc(Nome_Semana, Horario_Inicio, Horario_AlmocoInicio, Horario_AlmocoFim, Horario_Fim, Ind_Ativo) VALUES ('Quinta-Feira', '00:00:00', '00:00:00','00:00:00','00:00:00', 1)
+INSERT INTO FB_HorarioFunc(Nome_Semana, Horario_Inicio, Horario_AlmocoInicio, Horario_AlmocoFim, Horario_Fim, Ind_Ativo) VALUES ('Sexta-Feira', '00:00:00', '00:00:00','00:00:00','00:00:00', 1)
+INSERT INTO FB_HorarioFunc(Nome_Semana, Horario_Inicio, Horario_AlmocoInicio, Horario_AlmocoFim, Horario_Fim, Ind_Ativo) VALUES ('Sábado', '00:00:00', '00:00:00','00:00:00','00:00:00', 1)
+INSERT INTO FB_HorarioFunc(Nome_Semana, Horario_Inicio, Horario_AlmocoInicio, Horario_AlmocoFim, Horario_Fim, Ind_Ativo) VALUES ('Domingo', '01:00:00', '00:00:00','00:00:00','00:00:00', 1)
---
+++ fastBarberTG/scripts/scriptPages/_User.js
@@ -0,0 +1,37 @@
+﻿window.User = (function () {
+
+    var config = {
+        urls: {
+            showHorarios: ''
+        },
+    };
+
+    var init = function ($config) {
+        config = $config;
+    };
+
+
+    var showHorarios = function() {
+        $.get(config.urls.showHorarios).done(function(html) {
+            $("#user-requestdiv").show("slow").html(html);
+            $("#user-menu").hide("slow");
+        }).fail(function(msg) {
+            iziToast.error({
+                title: 'ERRO',
+                message: 'Erro na requisição',
+            });
+        });
+    };
+
+    var addDay = function (el) {
+        event.preventDefault();
+        $(el).addClass("checked");
+    }
+
+    return {
+        init: init,
+        showHorarios: showHorarios,
+        addDay: addDay
+    };
+})();
+
